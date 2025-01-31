016287c17584eb7355bdb52773916627fa83661c
kimdj1102 <djkim1102@naver.com>
2025-01-31 15:08:04 +0900

    Initial Commit

---
+++ Liars bar/Liars_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liars_barApp.swift
+//  Liars bar
+//
+//  Created by 김동준 on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Liars_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Liars barTests/Liars_barTests.swift
@@ -0,0 +1,17 @@
+//
+//  Liars_barTests.swift
+//  Liars barTests
+//
+//  Created by 김동준 on 1/31/25.
+//
+
+import Testing
+@testable import Liars_bar
+
+struct Liars_barTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
